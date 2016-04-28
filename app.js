3bd27de9cb44841f8dc98ae0c57df4978181ebb2
LanceChantiles <lance.chantiles@yale.edu>
2016-04-28 14:15:01 -0400

    Robot

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var port = process.env.PORT || 3000;
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
       res.render('index');
