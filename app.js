9c5ad2d3ad24c864c38b4d25b2ec1bd5ee99b2c6
LanceChantiles <lance.chantiles@yale.edu>
2016-04-28 14:07:11 -0400

    More stuff

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.get('/', function (req, res) {
       res.render('index');
 });
 
+app.get('/robots.txt', function (req, res) {
+      res.render('layouts/robots');
+});
+
+app.get('/mrw/class-is-done.gif', function (req, res) {
+      res.redirect('https://www.google.com/search?q=party+gif&espv=2&biw=1440&bih=750&site=webhp&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjW673z9bHMAhXjrYMKHdeZAKAQ_AUIBigB#imgrc=Ce_kIijZDzXLIM%3A');
+});
+
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
 });
